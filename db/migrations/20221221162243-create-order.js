7f8b33ea0ed5e9dbbe1f3ce8e72504201ad9f931
KirichenkoYV <hause99@gmail.com>
2022-12-21 19:52:37 +0300

    chore:init:db

---
+++ db/migrations/20221221162243-create-order.js
@@ -0,0 +1,40 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      phone: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      url: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Orders');
+  },
+};
--- index.js
+++ index.js
@@ -2,7 +2,24 @@ require('dotenv').config();
 require('@babel/register');
 
 const express = require('express');
+const db = require('./db/models');
 
 const app = express();
+const PORT = 3000;
 
-app.listen(3000);
+app
+  .listen(PORT)
+  .on('error', (error) => {
+    console.error('Ошибка при запуске веб-сервера');
+    console.error(error.message);
+  })
+  .on('listening', async () => {
+    console.log('Веб-сервер слушает порт,', PORT);
+    try {
+      await db.sequelize.authenticate({ logging: false });
+      console.log('БД подключена успешно');
+    } catch (error) {
+      console.error('Ошибка подключения БД');
+      console.error(error.message);
+    }
+  });
