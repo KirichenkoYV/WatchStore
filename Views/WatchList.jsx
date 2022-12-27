4ed416aa21079f2aa3dba489b660527017810566
Ibra <Ibrahim-1991@mail.ru>
2022-12-27 23:16:51 +0300

    Create watchListPage and NewContentPage

--- Views/WatchList.jsx
+++ Views/WatchList.jsx
@@ -2,21 +2,28 @@ const React = require('react');
 const Layout = require('./Layout');
 
 function WatchList({ watchList }) {
-  <Layout>
-    {watchList.map((watch) => (
-      <>
-        <title>{watch.dataValues.title}</title>
-        <img
-          className="d-block w-30 h-25"
-          width="50"
-          height="15"
-          src={watch.dataValues.imagePath}
-          alt="image1"
-        />
-        <text>{watch.dataValues.note}</text>
-        <small>{watch.dataValues.createdAt}</small>
-      </>
-    ))}
-  </Layout>;
+  return (
+    <Layout>
+      <h3>Редактирование контента</h3>
+      <br />
+      <a href="/admin/add-new-content">Добавить часы</a>
+      <br />
+      <br />
+      <ol className="js-watches">
+        {watchList.map((watch) => (
+          <li>
+            <div>
+              <img src={watch.dataValues.imagePath} alt={watch.dataValues.id} />
+              <div>
+                <h6>{watch.dataValues.title}</h6>
+                <p>{watch.dataValues.note}</p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ol>
+      
+    </Layout>
+  );
 }
 module.exports = WatchList;
