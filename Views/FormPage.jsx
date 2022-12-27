da8752200b8ec7dde37067bc5cbdb9ca4f129be0
Mariia <pravdova97@mail.ru>
2022-12-28 00:21:24 +0300

    change  header

--- Views/FormPage.jsx
+++ Views/FormPage.jsx
@@ -2,44 +2,46 @@ const React = require('react');
 
 module.exports = function FormPage() {
   return (
-    <form
-      method="POST"
-      action="/neworder"
-      className="js-creatOrder"
-      style={{ display: 'block' }}
-    >
-      <div>
-        Cоздать заказ:
-        <input
-          className="nameClient"
-          type="text"
-          placeholder="Ваше имя"
-          name="name"
-        />
-        <input
-          className="emailClient"
-          type="text"
-          placeholder="Ваша почта"
-          name="email"
-        />
-        <input
-          className="phoneClient"
-          type="text"
-          placeholder="Ваш номер телефона"
-          name="phone"
-        />
-        <input
-          className="urlClient"
-          type="text"
-          placeholder="Ссылка на понравившиеся часы"
-          name="url"
-        />
-        <a href="/">
-          <button type="submit" className="buttonSend">
-            Small button
-          </button>
-        </a>
-      </div>
-    </form>
+    <section>
+      <form
+        method="POST"
+        action="/neworder"
+        className="js-creatOrder"
+        style={{ display: 'block' }}
+      >
+        <div>
+          Cоздать заказ:
+          <input
+            className="nameClient"
+            type="text"
+            placeholder="Ваше имя"
+            name="name"
+          />
+          <input
+            className="emailClient"
+            type="text"
+            placeholder="Ваша почта"
+            name="email"
+          />
+          <input
+            className="phoneClient"
+            type="text"
+            placeholder="Ваш номер телефона"
+            name="phone"
+          />
+          <input
+            className="urlClient"
+            type="text"
+            placeholder="Ссылка на понравившиеся часы"
+            name="url"
+          />
+          <a href="/">
+            <button type="submit" className="buttonSend">
+              Small button
+            </button>
+          </a>
+        </div>
+      </form>
+    </section>
   );
 };
