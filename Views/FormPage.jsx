d908ab821e499b989cb571ef3b18e2877c38e2ec
Mariia <pravdova97@mail.ru>
2022-12-30 04:20:32 +0300

    style form and some cabitenAdmin

--- Views/FormPage.jsx
+++ Views/FormPage.jsx
@@ -3,42 +3,44 @@ const React = require('react');
 module.exports = function FormPage() {
   return (
     <div className="order" id="Order">
-      <form method="POST" action="/neworder" className="order__content">
-        <span>Create an order for a unique watch</span>
-        <input
-          className="nameClient form-control"
-          type="text"
-          placeholder="Your name"
-          name="name"
-          required
-        />
-        <input
-          className="emailClient form-control"
-          type="text"
-          placeholder="Email"
-          name="email"
-          required
-        />
-        <input
-          className="phoneClient form-control"
-          type="text"
-          placeholder="Phone"
-          name="phone"
-          required
-        />
-        <input
-          className="urlClient form-control"
-          type="text"
-          placeholder="Link to favorite watch"
-          name="url"
-          required
-        />
-        <a href="/">
-          <button type="submit" className="buttonSend">
-            Small button
-          </button>
-        </a>
-      </form>
+      <h1 className="order__title">Create an order for a unique watch</h1>
+      <div className="order__content">
+        <form method="POST" action="/neworder" className="order__item">
+          <input
+            className="nameClient form-control"
+            type="text"
+            placeholder="Your name"
+            name="name"
+            required
+          />
+          <input
+            className="emailClient form-control"
+            type="text"
+            placeholder="Email"
+            name="email"
+            required
+          />
+          <input
+            className="phoneClient form-control"
+            type="text"
+            placeholder="Phone"
+            name="phone"
+            required
+          />
+          <input
+            className="urlClient form-control"
+            type="text"
+            placeholder="Link to favorite watch"
+            name="url"
+            required
+          />
+          <a href="/">
+            <button type="submit" className="buttonSend">
+              SEND
+            </button>
+          </a>
+        </form>
+      </div>
     </div>
   );
 };
